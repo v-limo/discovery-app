2e29a92b9d2b39fa3dd7deb8a274b049211db911
Limo Vincent <48591371+LimoVincent@users.noreply.github.com>
2022-04-25 17:53:19 +0300

    fix :api health check

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -16,6 +16,14 @@ app.use(cors())
 app.use(express.json())
 
 // Set up routers
+app.get('/', (req, res) => {
+  res.json({
+    'OK : ✅': new Date().toISOString(),
+    example_resource:
+      'https://wolt-app.herokuapp.com/v1/api/discovery?lat=60.17091&lon=24.94101',
+  })
+})
+
 app.use('/api/v1/discovery', discoveryRouter)
 
 // Custom API error handler
